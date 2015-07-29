1225e5e65b02f5d81458982563e8e3257defc097
BlakeTurner <blake@strutta.com>
2015-07-29 15:30:48 +0100

    join builder

---
+++ lib/get/builders/join_builder.rb
@@ -0,0 +1,38 @@
+module Get
+  module Builders
+    class JoinBuilder < BaseBuilder
+      private
+
+      def class_args
+        {
+          base_table: @result_entity.symbolize,
+          join_table: @key,
+          store: Get.adapter.context_for_entity(@result_entity.singularize.symbolize)
+        }
+      end
+
+      def template_class(args)
+        Class.new(::Get::Db) do
+          include Get
+
+          class << self
+            attr_reader :base_table, :join_table
+          end
+
+          @base_table, @join_table, @store = args[:base_table], args[:join_table], args[:store]
+
+          def initialize(options)
+            @options = options.merge(with: self.class.join_table)
+            super(query_params)
+          end
+
+          private
+
+          def query_params
+            { join: @options }
+          end
+        end
+      end
+    end
+  end
+end
